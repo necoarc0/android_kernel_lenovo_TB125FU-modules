5e3c43c7ad5df28a94f9aa77393ccb3be3382751
Neco Arc <necoarc007@proton.me>
2024-12-01 12:40:41 +0100

    Fix DEFINE_PROC_SHOW_ATTRIBUTE to use mtk_common_gpu_utilization_show_open for GPU utilization

--- kernel_modules/gpu/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
+++ kernel_modules/gpu/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/platform/mtk_platform_common/mtk_platform_common.c
@@ -154,7 +154,7 @@ static int mtk_common_gpu_utilization_show(struct seq_file *m, void *v)
 
     return 0;
 }
-DEFINE_SHOW_ATTRIBUTE(mtk_common_gpu_utilization_show);
+DEFINE_PROC_SHOW_ATTRIBUTE(mtk_common_gpu_utilization_show_open);
 
 static int mtk_common_gpu_memory_show(struct seq_file *m, void *v)
 {
